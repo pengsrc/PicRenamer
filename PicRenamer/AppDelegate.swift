fbe721743db7535b209fc9920c7dc68d0937d598
Peng Jingwen <pengsrc@icloud.com>
2015-02-26 00:13:21 +0800

    StoryBoard
    Signed-off-by: Peng Jingwen <pengsrc@icloud.com>

--- PicRenamer/AppDelegate.swift
+++ PicRenamer/AppDelegate.swift
@@ -10,6 +10,9 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true;
+    }
     
 }
