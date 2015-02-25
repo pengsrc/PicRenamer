84faa61f5e907b8a5512dafa1ccd6d9a6266fc60
Peng Jingwen <pengsrc@icloud.com>
2015-02-25 10:11:57 +0800

    Switching to Swift
    Signed-off-by: Peng Jingwen <pengsrc@icloud.com>

---
+++ PicRenamer/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  PicRenamer
+//
+//  Created by Peng Jingwen on 2015-02-25.
+//  Copyright (c) 2015 PrettyX. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+}
