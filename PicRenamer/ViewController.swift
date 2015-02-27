2a567fbfa1ed73c7e6ebd0bb3a26c3abfc3ca6f7
Peng Jingwen <pengsrc@icloud.com>
2015-02-27 10:32:35 +0800

    Update UI
    Signed-off-by: Peng Jingwen <pengsrc@icloud.com>

---
+++ PicRenamer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PicRenamer
+//
+//  Created by Peng Jingwen on 2015-02-26.
+//  Copyright (c) 2015 PrettyX. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController, NSTableViewDataSource, NSTabViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    
+    
+}
